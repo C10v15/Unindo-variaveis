4df4b164b9d52b976c4462f40df695a74a787dd1
Clovis Santiago <sant_sivolc@hotmail.com>
2022-09-29 10:12:24 -0300

    primeiro comit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+alert("Seja bem vindo(a) aventureiro(a)!")
+const retornoUsuario = confirm ("Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?")
+
+if(retornoUsuario){
+    alert("Ótimo! Nós temos as melhores camas de toda a região!") 
+}else{
+alert("Que pena! Você parecia ser uma pessoa mais legal :(")
+}
